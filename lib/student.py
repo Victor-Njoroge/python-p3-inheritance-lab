307ef51b13342b98662b90e65343ac8a638eb506
Victor Njoroge <victornjoroge2020@gmail.com>
2023-09-01 14:16:51 +0300

    done

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,13 @@
 from user import User
 
 class Student(User):
+
+    def __init__(self,first_name,last_name,knowledge=[]):
+        super().__init__(first_name,last_name)
+        self.knowledge=knowledge
     
-    def learn(self):
-        pass
+    def learn(self,string="Python"):
+        self.string=string
+        self.knowledge.append(self.string)
+        return self.knowledge
+    
--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 
 class User:
-    pass
+    def __init__(self,first_name,last_name):
+        self.first=first_name
+        self.last=last_name
